0f3a507432744b19fc50062ce6991246bbbd50b1
liukai <kai8406@gmail.com>
2012-06-30 13:02:01 +0800

    add javascript function about next button and back button.

--- WebContent/js/cmop.js
+++ WebContent/js/cmop.js
@@ -1,14 +1,21 @@
 
-function displayParameter(){
+/**
+ * 将输入的各种参数插入到最后的详情table中
+ * 注意对比table中td 的ID
+ */
+function displayInputParameter(){
 	
 	//标题
 	$("#td_title").html($("#title").val());
+	
 	//起始日期
 	$("#td_time").html( $("#serviceStart").val() + '&nbsp;至&nbsp;' + $("#serviceEnd").val());
+	
 	//申请用途
 	$("#td_usage").html($("#usage").val());
+	
 	//资源类型
-	var resourceType = $("input[name='resourceType'][checked]").val();
+	resourceType = $("input[name='resourceType'][checked]").val();
 	if(resourceType === 1){
 		$("#td_resourceType").html("生产资源");
 	}else if(resourceType ===2){
@@ -22,5 +29,31 @@ function displayParameter(){
 	user = $("#accountUser").val();
 	visitHost = $("#visitHost").val();
 	$("#td_inVpnItem").html("<code>账号:</code>"+account+"<code>使用人:</code>"+user+"<code>需要访问主机:</code>"+visitHost);
+	
+}
+
+/**
+ * 申请页面中,点击"下一步"和"后退"时切换Tab
+ * ul的ID	:		myTab
+ * 下一步按钮ID:	nextStep
+ * 返回按钮ID:		backStep
+ */
+function switchTab(){
+	
+	var nextSteps = $("a[id^='nextStep']");
+	nextSteps.click(function(){
+		$('#myTab li:eq('+ (nextSteps.index(this) + 1) +') a').tab('show'); 
+		displayInputParameter();
+	 });
+	
+	var backSteps = $("a[id^='backStep']");
+	backSteps.click(function(){
+		
+		$('#myTab li:eq('+backSteps.index(this)+') a').tab('show'); // Select 1 tab (0-indexed)
+		
+		displayInputParameter();
+	 });
+	
+	
 	
 }
